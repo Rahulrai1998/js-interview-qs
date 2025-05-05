56aa7c213a9b99670ca1973e35e9ffd465090210
Rahul Kumar <rahulrai2822march@gmail.com>
2025-05-05 20:37:20 +0530

    topic complete

--- var-let-const.js
+++ var-let-const.js
@@ -1,57 +1,65 @@
-// DIFFERENCE B/W VAR, LET AND CONST
+// // DIFFERENCE B/W VAR, LET AND CONST
 
-//SCOPE:
-console.log(a); // hoisted and initialized with undefined
-{
-  var a = 2;
-  const b = 3;
-  let c = 3;
-  console.log(a, b, c);
-}
-console.log(a); // 2 (var is function scope)
-console.log(b); // not accessible(const is block-scope)
-console.log(c); // not accessible(let is block-scope)
+// //SCOPE:
+// console.log(a); // hoisted and initialized with undefined
+// {
+//   var a = 2;
+//   const b = 3;
+//   let c = 3;
+//   console.log(a, b, c);
+// }
+// console.log(a); // 2 (var is function scope)
+// console.log(b); // not accessible(const is block-scope)
+// console.log(c); // not accessible(let is block-scope)
 
-//SHADOWING
-let message = "Hello";
-function greet() {
-  let message = "Hi"; // shadows the outer `message`
-  console.log(message); // Output: Hi
-}
-greet();
-console.log(message); // Output: Hello
+// //SHADOWING
+// let message = "Hello";
+// function greet() {
+//   let message = "Hi"; // shadows the outer `message`
+//   console.log(message); // Output: Hi
+// }
+// greet();
+// console.log(message); // Output: Hello
 
-//LEAGAL AND ILLEGAL SHADOWING
-function test() {
-  var a = "hello";
-  let b = "goodbye";
-  if (true) {
-    let a = "hi"; // shadowing var with let: LEGAL SHADOWING
-    var b = "new"; // shadowing let with var : ILLEGAL SHADOWING
-    console.log(a);
-    console.log(b);
-  }
-}
+// //LEAGAL AND ILLEGAL SHADOWING
+// function test() {
+//   var a = "hello";
+//   let b = "goodbye";
+//   if (true) {
+//     let a = "hi"; // shadowing var with let: LEGAL SHADOWING
+//     var b = "new"; // shadowing let with var : ILLEGAL SHADOWING
+//     console.log(a);
+//     console.log(b);
+//   }
+// }
+
+// //DECLARATIONS (IN THE SAME SCOPE)
+// var a
+// var a // CAN BE RE-DECLARED
+
+// let a
+// let a // SYNTAX-ERROR
+
+// const a
+// const a // SYNTAX-ERROR and MISSING-INITIALIZER
+
+// //DECLARATIONS (IN DIFFERENT SCOPE)
+// let a
+// {
+//     let a // fine
+// }
+
+// //DECLARATIONS WITHOUT INITIIALIZATIONS
+// var a //fine
+// let b //fine
+// const c //SyntaxError: Missing initializer
 
 
-//DECLARATIONS (IN THE SAME SCOPE) 
-var a
-var a // CAN BE RE-DECLARED
 
-let a
-let a // SYNTAX-ERROR
+//HOISTING 
+console.log(a); // undefined (a variable is hoisted and undefined is assigned to it)
+var a = 10;
 
-const a
-const a // SYNTAX-ERROR and MISSING-INITIALIZER
-
-//DECLARATIONS (IN DIFFERENT SCOPE)
-let a
-{
-    let a // fine
-}
-
-//DECLARATIONS WITHOUT INITIIALIZATIONS
-var a //fine
-let b //fine
-const c //SyntaxError: Missing initializer
-
+console.log(b, c); // ReferenceError (b and c are hoisted(in TEMPORAL DEAD ZONE) but not initialized)
+const b = 3;
+let c = 5;
