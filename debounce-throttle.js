93449b26c33f8904a56e6cb62704a1510106ea66
Rahul Kumar <rahulrai2822march@gmail.com>
2025-06-10 02:09:20 +0530

    debounce and throttle polyfills

--- debounce-throttle.js
+++ debounce-throttle.js
@@ -9,7 +9,34 @@ let triggeredCountThr = 0;
 
 //debounce and throttle Polyfills
 
+const myDebounce = (callback, delay) => {
+  let timer;
+  return function (...args) {
+    //this clears the previous timer
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(() => {
+      callback(...args);
+    }, delay);
+  };
+};
 
+const myThrottle = (callback, delay) => {
+  let last = 0;
+  return function (...args) {
+    let now = new Date().getTime();
+    if (now - last < delay) return;
+    last = now;
+    return callback(...args);
+  };
+};
+
+const debounce = myDebounce(() => {
+  triggeredElmDeb.innerText = ++triggeredCountDeb;
+}, 1800);
+
+const throttle = myThrottle(() => {
+  triggeredElmThr.innerText = ++triggeredCountThr;
+}, 5000);
 //LODASH USAGE
 const debounceTriggeredCount = _.debounce(() => {
   triggeredElmDeb.innerText = ++triggeredCountDeb;
@@ -21,6 +48,8 @@ const throttleTriggeredCount = _.throttle(() => {
 
 btnElm.addEventListener("click", () => {
   btnPressedCountElm.innerText = ++btnPressedCount;
-  debounceTriggeredCount();
-  throttleTriggeredCount();
+  debounce();
+  // debounceTriggeredCount();
+  // throttleTriggeredCount();
+  throttle();
 });
