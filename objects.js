ee4a9589d278b1666dd725a016f6c0eb0f81c0ce
Rahul Kumar <rahulrai2822march@gmail.com>
2025-07-30 07:14:51 +0530

    object updates

--- objects.js
+++ objects.js
@@ -149,5 +149,80 @@ console.log(newGreet.greeting, greet);
 //in this example each obj hold different addresses despite the same value
 console.log({ a: 1 } == { a: 1 }); // false
 console.log({ a: 1 } === { a: 1 }); //false
- 
-//
+
+//12. output
+let person = { name: "Lydia" };
+const members = [person];
+// person = null; // it won't change the array value
+person.name = null; // it will affect the array
+console.log(members); // [{name:null}]
+
+//13. Output
+const value = { number: 10 };
+//spread operator clones the object, by making shallow copy of it rather than assigning reference. Hence there is not affect on original object on modifying the clone
+const multiply = (x = { ...value }) => {
+  console.log((x.number *= 2));
+};
+
+multiply(); //20, will use default value
+multiply(); //20, will use default value
+multiply(value); //20, pass by reference
+multiply(value); //40, pass by reference
+
+//14. Output
+function changeAgeAndReference(person) {
+  //memory address of personObj1 is copied into person para
+  person.age = 25; // it changes the original array
+  //now the person is pointing over new memory address, but personObj1 is still intact
+  person = {
+    name: "Raj",
+    age: 56,
+  };
+  return person;
+}
+const personObj1 = {
+  name: "Alex",
+  age: 25,
+};
+const personObj2 = changeAgeAndReference(personObj1); // reference is passed
+console.log(personObj1); //{name: 'Alex', age: 25}
+console.log(personObj2); //{name: 'Raj', age: 56}
+
+//shallow and deep copy
+const original = { name: "Alex", address: { city: "Delhi" } };
+
+// Shallow copy
+const shallow = { ...original };
+shallow.address.city = "Mumbai";
+
+//here, city property still pointing over the address of original object's reference.
+console.log(original.address.city); // "Mumbai" — shared reference!
+
+// Deep copy
+const deep = JSON.parse(JSON.stringify(original));
+deep.address.city = "Kolkata";
+
+console.log(original.address.city); // "Mumbai" — unaffected
+
+//15. How to deep copy or clone an object
+let origin = {
+  name: "Rahul Kumar",
+  age: 25,
+  address: {
+    city: "Patna",
+  },
+};
+
+//1st way, shallow copy only
+const originClone = Object.assign({}, origin);
+originClone.name = "Ramesh"; // it won't affect the origin object name
+originClone.address.city = "Kolkata"; //It wil affect the nested address of origin obj
+console.log(origin.address.city); //Kolkata
+
+//2nd way, shallow copy
+const spreadCopy = { ...origin }; // same behaviour as above
+
+//deep copy
+const deepClone = JSON.parse(JSON.stringify(origin));
+deepClone.address.city = "New York"; // it won't affect the origin's nested object
+console.log(origin.address.city); //Kolkata
